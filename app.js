40dc762b07c3d03435399ce688a4353099a25e9f
Sardorbek Olimov <sardor.20070731@gmail.com>
2023-09-24 21:19:50 +0500

    changed home page design

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const formModal = document.getElementById("formModal");
 const overlayBg = document.getElementById("overlayBg");
 const closeBtn = document.getElementById("closeBtn");
 const registerForm = document.getElementById("registerForm");
+const minUserImage = document.getElementById("min-user-image");
 
 const closeModal = () => {
   formModal.classList.add("d-none");
@@ -22,5 +23,10 @@ closeBtn.onclick = closeModal;
 registerForm.onsubmit = (e) => {
   e.preventDefault();
 
-  window.location.href = "finalStep.html"
+  window.location.href = "finalStep.html";
 };
+
+let width = screen.width;
+if (width <= 480) {
+  minUserImage.src = "./img/3.png";
+}
